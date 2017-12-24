af7aeb967c26951a38b1e5c717260f09700d4522
Oshadha Gunasekara <ovg@andrew.cmu.edu>
2017-12-24 01:13:53 -0700

    Add initial catkin_ws

---
+++ catkin_ws/build/controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/oshadha/hsa2/catkin_ws/src/controller
+# Build directory: /home/oshadha/hsa2/catkin_ws/build/controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/odometry/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/oshadha/hsa2/catkin_ws/src/odometry
+# Build directory: /home/oshadha/hsa2/catkin_ws/build/odometry
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
