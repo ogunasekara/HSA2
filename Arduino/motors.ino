bf04098a104396645c01d011798a1265c8067db2
osguus <ovg@andrew.cmu.edu>
2018-01-04 14:22:15 -0700

    Add odometry msg

--- Arduino/motors.ino
+++ Arduino/motors.ino
@@ -1,19 +1,22 @@
 #include <ros.h>
+#include <ros/time.h>
 #include <std_msgs/Int32.h>
-#include <string.h>
+#include <odometry/Encoder.h>
 
 // MACRO DEFINITIONS
 #define PWM_L 10
 #define DIR_L 8
-// #define ENCA_L 2
-// #define ENCB_L
+#define ENCA_L 2
+#define ENCB_L 4
 #define PWM_R 11
 #define DIR_R 7
-// #define ENCA_R 3
-// #define ENCB_R
+#define ENCA_R 3
+#define ENCB_R 5
 
 // GLOBAL VARIABLES
 ros::NodeHandle nh;
+volatile unsigned long leftCount = 0;
+volatile unsigned long rightCount = 0;
 
 void leftMotorCallback(const std_msgs::Int32& msg){
   // extract left velocity from msg
@@ -57,8 +60,43 @@ void rightMotorCallback(const std_msgs::Int32& msg){
   analogWrite(PWM_R, abs(velR));
 }
 
+void leftEncEvent(){
+  if (digitalRead(ENCA_L) == HIGH) {
+    if (digitalRead(ENCB_L) == LOW) {
+      leftCount++;
+    } else {
+      leftCount--;
+    }
+  } else {
+    if (digitalRead(ENCB_L) == LOW) {
+      leftCount--;
+    } else {
+      leftCount++;
+    }
+  }
+}
+
+void rightEncEvent(){
+  if (digitalRead(ENCA_R) == HIGH) {
+    if (digitalRead(ENCB_R) == LOW) {
+      rightCount++;
+    } else {
+      rightCount--;
+    }
+  } else {
+    if (digitalRead(ENCB_R) == LOW) {
+      rightCount--;
+    } else {
+      rightCount++;
+    }
+  }
+}
+
 ros::Subscriber<std_msgs::Int32> lmotor_sub("left_motor", &leftMotorCallback);
 ros::Subscriber<std_msgs::Int32> rmotor_sub("right_motor", &rightMotorCallback);
+
+odometry::Encoder enc_msg;
+ros::Publisher enc_pub("encoders", &enc_msg);
 
 // MAIN FUNCTIONS
 void setup(){
@@ -69,20 +107,30 @@ void setup(){
   pinMode(DIR_R, OUTPUT);
 
   // Initialize encoder pins
-  // pinMode(ENCA_L, INPUT);
-  // pinMode(ENCB_L, INPUT);
-  // pinMode(ENCA_R, INPUT);
-  // pinMode(ENCB_R, INPUT);
-  // digitalWrite(ENCA_L, LOW);
-  // digitalWrite(ENCA_R, LOW);
+  pinMode(ENCA_L, INPUT);
+  pinMode(ENCB_L, INPUT);
+  pinMode(ENCA_R, INPUT);
+  pinMode(ENCB_R, INPUT);
+  digitalWrite(ENCA_L, LOW);
+  digitalWrite(ENCA_R, LOW);
+
+  // initialize hardware interrupts
+  attachInterrupt(0, leftEncEvent, CHANGE);
+  attachInterrupt(1, rightEncEvent, CHANGE);
 
   // Initialize ROS
   nh.initNode();
   nh.subscribe(lmotor_sub);
   nh.subscribe(rmotor_sub);
+  nh.advertise(enc_pub);
 }
 
 void loop(){
+  enc_msg.header.stamp = nh.now();
+  enc_msg.left_enc = leftCount;
+  enc_msg.right_enc = rightCount;
+  enc_pub.publish(&enc_msg);
+
   delay(1);
   nh.spinOnce();
 }
--- catkin_ws/src/odometry/src/odometry_node.cpp
+++ catkin_ws/src/odometry/src/odometry_node.cpp
@@ -1,46 +1,59 @@
 #include <geometry_msgs/Pose.h>
+#include <odometry/Encoder.h>
 #include <std_msgs/String.h>
 #include <string.h>
+#include <math.h>
+
+// GLOBAL CONSTANTS
+#define TICKS_PER_REV 1000
+#define DIST_PER_REV 0.4785
+#define WIDTH 10
 
 // GLOBAL VARIABLES
 int x, y, th;
 int prev_enc_l, prev_enc_r;
-bool init;
+int prev_time;
+bool init = false;
 
-void encCallback(const std_msgs::String::ConstPtr& msg)
+void encCallback(const odometry::Encoder::ConstPtr& msg)
 {
-  char *charBuf = msg.data.c_str();
-  const char s[2] = "/";
-
   // get encoder values
-  int left_enc = atoi(strtok(charBuf, s));
-  int right_enc = atoi(strtok(NULL, s));
+  int left_enc = msg.left_enc;
+  int right_enc = msg.right_enc;
+  float cur_time = msg.header.stamp.secs + (msg.header.stamp.nsecs/1E9);
 
   if(!init){
+    // initialize globals
+    x = 0;
+    y = 0;
+    th = 0;
     prev_enc_l = left_enc;
     prev_enc_r = right_enc;
+    prev_time = cur_time;
     init = true;
   }
   else{
-        
+    float dt = cur_time - prev_time;
+    float v_l = (((left_enc - prev_enc_l)/TICKS_PER_REV)*DIST_PER_REV)/dt;
+    float v_r = (((right_enc - prev_enc_r)/TICKS_PER_REV)*DIST_PER_REV)/dt;
+    float V = (v_r + v_l)/2.0;
+    float w = (v_r - v_l)/WIDTH;
+    th += w * dt;
+    x += V * cos(th);
+    y += V * sin(th);
+    th += w * dt;
   }
 }
 
 int main(int argc, char **argv)
 {
-  // initialize globals
-  init = false;
-  x = 0;
-  y = 0;
-  th = 0;
-
   // set up ROS
   ros::init(argc, argv, "odom");
   ros::NodeHandle n;
 
   // set up subscribers and publishers
   ros::Publisher odom_pub = n.advertise<geometry_msgs::Pose>("pose", 1000);
-  ros::Subscriber enc_sub = n.subscribe<std_msgs::String>("odom/pose", 1000, encCallback);
+  ros::Subscriber enc_sub = n.subscribe<odometry::Encoder>("odom/pose", 1000, encCallback);
   ros::Rate loop_rate(10);
 
   while(ros::ok()){
