f3888a7d5696d2c8da62ff13af4fc7a795a7fc5d
Oshadha <oshadha123@gmail.com>
2020-03-29 22:28:00 -0700

    update parameters

--- src/parser/src/teleop_parser.py
+++ src/parser/src/teleop_parser.py
@@ -7,34 +7,43 @@ from sensor_msgs.msg import Joy
 
 class TeleopParser(object):
     def __init__(self):
+        # get parameters
+        self.lin_vel_max = rospy.get_param('lin_vel_max', 0.5)
+        self.ang_vel_scale = rospy.get_param('ang_vel_scale', 2.5)
+        
         self.v = 0
         self.w = 0
-        self.L = 0.338
+        self.L = 0.3937
 
         self.left_motor_vel_pub = rospy.Publisher('left_motor/cmd/vel', Float32, queue_size=10)
         self.right_motor_vel_pub = rospy.Publisher('right_motor/cmd/vel', Float32, queue_size=10)
         rospy.Subscriber("joy", Joy, self.joy_callback)
-        
+
         rospy.init_node('teleop_parser', anonymous=True)
-        rate = rospy.Rate(10) # 10hz
-        while not rospy.is_shutdown():
-            vels = self.convert_vels()
-            self.left_motor_vel_pub.publish(vels[0])
-            self.right_motor_vel_pub.publish(vels[1])
-            rate.sleep()
+        rospy.spin()
+
+        # rate = rospy.Rate(10) # 10hz
+        # while not rospy.is_shutdown():
+            
+        #     rate.sleep()
 
     def joy_callback(self, msg):
         if msg.buttons[0] == 1:
-            self.v = 0.2
+            self.v = self.lin_vel_max
         elif msg.buttons[1] == 1:
-            self.v = -0.2
+            self.v = -self.lin_vel_max
         else:
             self.v = 0
 
         if abs(msg.axes[0]) > 0.05:
-            self.w = msg.axes[0] * 0.5
+            self.w = msg.axes[0] * self.ang_vel_scale
         else:
             self.w = 0
+
+        vels = self.convert_vels()
+        
+        self.right_motor_vel_pub.publish(vels[1])
+        self.left_motor_vel_pub.publish(vels[0])
         
     def convert_vels(self):
         vl = (2.0 * self.v - self.L * self.w) / 2.0
